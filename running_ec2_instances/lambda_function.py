96602fde890041faaf15fee8f70542ca7e39cc15
Zaire Ali <zaireali649@gmail.com>
2022-12-19 20:31:28 -0500

    Update lambda_function.py

--- running_ec2_instances/lambda_function.py
+++ running_ec2_instances/lambda_function.py
@@ -3,7 +3,23 @@ import boto3
 
 def lambda_handler(event, context):
     
+    ec2 = boto3.client('ec2')
+    
+    running_ids = []
+
+    response = ec2.describe_instances()
+    
+    reservations = response['Reservations']
+    
+    for reservation in reservations:
+        instances = reservation['Instances']
+    
+        for instance in instances:
+            if ('running' in instance['State']['Name']):
+                print(instance['InstanceId'])
+                running_ids.append(instance['InstanceId'])
     
     return {
-        'body': json.dumps([])
+        'statusCode': 200,
+        'body': json.dumps(running_ids)
     }

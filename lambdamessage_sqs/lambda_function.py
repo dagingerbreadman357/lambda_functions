5020c70a00c466d1812af5170dae970db80b668a
Keon <mr.edwards.kj@gmail.com>
2023-02-24 10:31:26 -0800

    fixed formatting of code again

--- lambdamessage_sqs/lambda_function.py
+++ lambdamessage_sqs/lambda_function.py
@@ -7,15 +7,11 @@ from datetime import datetime, timezone
 
 def lambda_handler(event, context):
 
-    sqs = boto3.resource('sqs')                                                     # setting sqs as the resource
-    
-    local = datetime.now(dateutil.tz.gettz('US/Pacific'))                           # obtain date and time in Pacific Standard Time
-    
-    time = local.strftime('%I:%M%p on %B %d, %Y')                                   # local time
-    
+    sqs = boto3.resource('sqs')                                                     # setting sqs as the resource    
+    local = datetime.now(dateutil.tz.gettz('US/Pacific'))                           # obtain date and time in Pacific Standard Time    
+    time = local.strftime('%I:%M%p on %B %d, %Y')                                   # local time    
     queue = sqs.Queue(
         url='https://sqs.us-west-2.amazonaws.com/026023833752/KingSQS_Time')        # enter the URL of queue
-
     
     queue.send_message(MessageBody=time)                                            # will send a message to queue with current time and date in PST
 
